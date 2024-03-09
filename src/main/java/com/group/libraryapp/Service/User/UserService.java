e9a97e82543227520db974829ae62df68ab7fb00
nanami-tomoe <npnp123npnp123@gmail.com>
2024-03-10 05:19:29 +0900

    Controller 3단 분리

---
+++ src/main/java/com/group/libraryapp/Service/User/UserService.java
@@ -0,0 +1,42 @@
+package com.group.libraryapp.Service.User;
+
+import com.group.libraryapp.Repository.user.UserRepository;
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(JdbcTemplate jdbcTemplate) {
+        userRepository = new UserRepository(jdbcTemplate);
+    }
+
+    public void saveUser(UserCreateRequest request) {
+        userRepository.saveUser(request.getName(), request.getAge());
+    }
+
+    public List<UserResponse> getUsers() {
+        return userRepository.getUserResponses();
+    }
+
+    public void updateUser(UserUpdateRequest request) {
+        if (userRepository.IsUserNotExist(request.getId())) {
+            throw new IllegalArgumentException();
+        }
+
+        userRepository.updateUserName(request.getName(), request.getId());
+    }
+
+    public void deleteUser(String name) {
+        if (userRepository.IsUserNotExist(name)) {
+            throw new IllegalArgumentException();
+        }
+
+        userRepository.deleteUserByName(name);
+    }
+}

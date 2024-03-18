bb3455467b92903af4e620a7803574a0539a7f62
nanami-tomoe <npnp123npnp123@gmail.com>
2024-03-18 21:07:42 +0900

    트랜잭션 적용과 영속성 컨텍스트

--- src/main/java/com/group/libraryapp/Service/User/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/Service/User/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,25 +19,32 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
-
+    // 아래 있는 함수가 시작될 때 start transaction;을 해준다 (트랜잭션을 시작!)
+    // 함수가 예외 없이 잘 끝났다면 commit
+    // 문제가 있다면 rollback
+    // 트랜잭션이 시작할 때 영속성 컨텍스트 시작, 종료될 때 종료
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         userRepository.save(new User(request.getName(), request.getAge()));
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponse::new)
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         User user = userRepository.findById(request.getId())
                 .orElseThrow(IllegalArgumentException::new);
 
         user.updateName(request.getName());
-        userRepository.save(user);
+//        userRepository.save(user); 영속성 컨텍스트가 종료될 때 자동으로 저장해준다
     }
 
+    @Transactional
     public void deleteUser(String name) {
         User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
 
